9e99125efb478d2672de7aeb9de42d520a70889b
Anton Stubenbord <anton.stubenbord@protonmail.com>
2023-02-17 00:01:32 +0100

    feat: remove intl_utils, adapt install_dependencies script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,4 +6,4 @@ flutter pub run build_runner build --delete-conflicting-outputs
 popd
 flutter pub get
 flutter pub run build_runner build --delete-conflicting-outputs
-flutter pub run intl_utils:generate
+flutter gen-l10n
