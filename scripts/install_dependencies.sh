44d9b74fb3f406181ef4cf9d6049a6d5bc5b7aad
Anton Stubenbord <anton.stubenbord@protonmail.com>
2023-02-22 15:54:20 +0100

    fix: Remove generation from pubspec, update install script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,5 +5,5 @@ flutter pub get
 flutter pub run build_runner build --delete-conflicting-outputs
 popd
 flutter pub get
-flutter pub run build_runner build --delete-conflicting-outputs
 flutter gen-l10n
+flutter pub run build_runner build --delete-conflicting-outputs
