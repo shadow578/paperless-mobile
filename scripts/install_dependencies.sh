e7b095e6901155fc85d4d939568ee015854e5b63
Anton Stubenbord <anton.stubenbord@protonmail.com>
2023-05-12 10:37:47 +0200

    chore: Update versions, add freezed files to gitignore

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,8 +2,8 @@
 pushd ../
 pushd packages/paperless_api
 flutter pub get
-flutter pub run build_runner build --delete-conflicting-outputs
+dart run build_runner build --delete-conflicting-outputs
 popd
 flutter packages pub get
 flutter gen-l10n
-flutter pub run build_runner build --delete-conflicting-outputs
+dart run build_runner build --delete-conflicting-outputs
