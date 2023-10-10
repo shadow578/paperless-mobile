2484bd2c7c6beb1abc48112a145c76e089b7418a
Anton Stubenbord <anton.stubenbord@protonmail.com>
2023-10-10 19:22:41 +0200

    feat: add changelogs, update old changelogs, update build scripts

---
+++ lib/routes/typed/top_level/changelog_route.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:paperless_mobile/features/changelogs/view/changelog_dialog.dart';
+import 'package:paperless_mobile/routes/navigation_keys.dart';
+import 'package:paperless_mobile/routes/utils/dialog_page.dart';
+
+part 'changelog_route.g.dart';
+
+@TypedGoRoute<ChangelogRoute>(path: '/changelogs)')
+class ChangelogRoute extends GoRouteData {
+  static final $parentNavigatorKey = rootNavigatorKey;
+  @override
+  Page<void> buildPage(BuildContext context, GoRouterState state) {
+    return DialogPage(
+      builder: (context) => const ChangelogDialog(),
+    );
+  }
+}
---
+++ scripts/update_changelogs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+__script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+__target_dir=$__script_dir/../assets/changelogs
+readonly __script_dir
+# Receives locale as first argument
+function mergeChangelogs () {
+    __target_file=$__target_dir/changelogs_$1.md
+    rm -f $__target_file
+    touch $__target_file
+    ls $__script_dir/../android/fastlane/metadata/android/$1/changelogs/[0-9]*.txt | tac | while read f; do
+        __build_number="${f%.*}"
+        echo "# $(basename -- $__build_number)" >> $__target_file
+        cat $f >> $__target_file
+        printf "\n\n" >> $__target_file
+    done
+}
+
+mergeChangelogs 'en-US'
+mergeChangelogs 'de-DE'
+
