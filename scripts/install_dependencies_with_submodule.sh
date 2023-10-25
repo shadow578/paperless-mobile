b50db64705cf065e01c3a35fb2d57a1702e7e078
Anton Stubenbord <anton.stubenbord@protonmail.com>
2023-10-25 19:08:27 +0200

    fix: Update path to flutter submodule

--- scripts/install_dependencies_with_submodule.sh
+++ scripts/install_dependencies_with_submodule.sh
@@ -8,12 +8,12 @@ readonly __script_dir
 pushd "$__script_dir/../"
 
 pushd packages/paperless_api
-flutter/bin/flutter packages pub get
-flutter/bin/dart run build_runner build --delete-conflicting-outputs
+../../flutter/bin/flutter packages pub get
+../../flutter/bin/dart run build_runner build --delete-conflicting-outputs
 popd
 
 pushd packages/mock_server
-flutter/bin/flutter packages pub get
+../../flutter/bin/flutter packages pub get
 popd
 
 flutter/bin/flutter packages pub get
