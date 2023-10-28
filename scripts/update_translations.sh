7e2108a87ebeea9c6ccd71bee768d12ecd5849da
Anton Stubenbord <anton.stubenbord@protonmail.com>
2023-10-28 11:50:20 +0200

    chore: Cleanup scripts

--- scripts/update_translations.sh
+++ scripts/update_translations.sh
@@ -4,10 +4,9 @@ set -Eeuo pipefail
 __script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 readonly __script_dir
 
+cd "$__script_dir/../"
 echo "Updating source language..."
-crowdin download sources --identity=../crowdin_credentials.yml --config ../crowdin.yml --preserve-hierarchy
+crowdin download sources --identity=crowdin_credentials.yml --preserve-hierarchy
 echo "Updating translations..."
-crowdin download --identity=../crowdin_credentials.yml --config ../crowdin.yml
-pushd "$__script_dir/../"
+crowdin download --identity=crowdin_credentials.yml
 flutter gen-l10n
-popd
